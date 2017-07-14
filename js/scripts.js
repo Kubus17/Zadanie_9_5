325cee7ec97450389ff89ad1438aa9d4b09321c1
LAPTOP-57J009DG\piotr <kubus-o_2@tlen.pl>
2017-07-14 10:33:51 +0200

    Inicjuję zadanie 9_5

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+var k = document.getElementsByClassName('button');
+var menyLength = k.length;
+var textButton;
+for (var i = 0; i < menyLength; i++){
+
+     k[i].innerText = "Button nr. " + i;
+
+ }
